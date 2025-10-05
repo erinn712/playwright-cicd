cd2fb50e2a841e966b363228c5df872d6faed86a
Ada <erinnliao@gmail.com>
2025-10-05 21:04:46 +0800

    add allure report to github actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig, devices } from '@playwright/test';
+import os from 'os';
 
 export default defineConfig({
   testDir: './tests',
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['allure-playwright', {
+      environmentInfo: {
+        os_platform: os.platform(),
+        os_release: os.release(),
+        os_version: os.version(),
+        node_version: process.version,
+      }
+    }]
+  ],
   use: {
     screenshot: 'on',
     video: 'on',
